579152a98e71201db9eb4d33d36128f58f53c91a
ashindo <shindo@sikmi.com>
2023-06-09 18:09:38 +0900

    to chapter 5

---
+++ lib/rgb.rb
@@ -0,0 +1,20 @@
+def to_hex(r, g, b)
+    [r, g, b].sum('#') do |n|
+        n.to_s(16).rjust(2, '0')
+    end
+
+    # hex = '#'
+    # [r, g, b].each do |n|
+    #     hex += n.to_s(16).rjust(2, '0')
+    # end
+    # hex
+end
+
+def to_ints(hex)
+    hex.scan(/\w\w/).map(&:hex)
+
+    # r, g, b = hex[1..2], hex[3..4], hex[5..6]
+    # [r, g, b].map do |m|
+    #     m.hex
+    # end
+end
--- practice.rb
+++ practice.rb
@@ -3,7 +3,7 @@ puts a*100
 puts b # これは100のまま
 puts "オブチェクトIDをチェック"
 puts a.object_id
-puts b.object_id # IDは同じなんだ...
+puts b.object_id # IDは同じなんだ... → イミュータブルだからだった。
 
 puts c = d = 'hello'
 puts c.upcase!
@@ -46,4 +46,39 @@ puts some_method
 def greet = 'Hello!'
 puts greet
 
+# sumメソッド
+chars = ['a', 'b', 'c']
+puts chars.sum('') # 文字列の場合は初期値を設定しないとエラーになる
+num = [1,2,4]
+puts num.sum # 数値の場合は初期値がなくても動く
+# join
+puts num.join # 数値もto_sで文字列変換されてから連結される
 
+# 添字付きブロック
+fruits = ['apple', 'orange', 'melon']
+fruits.each.with_index {|fruit, i| puts "#{i}: #{fruit}"} # each_with_indexと同じ結果
+
+# 番号指定パラメータ
+['japan', 'us', 'italy'].map.with_index{|country,n| [n,country]} # 配列とwith_indexの変数を2つ||内で宣言している
+['japan', 'us', 'italy'].map.with_index{[_2, _1]} # 番号指定パラメータを使うと変数宣言がいらなくなる
+
+# キーワード引数の ...
+def foo(a, ...) # 第一引数だけ取り出して
+    bar(a*100, ...) # 別処理をすることができる
+end
+
+def bar(a, b, c: 1)
+    puts "a=#{a}, b=#{b}, c=#{c}"
+end
+
+puts foo(10, 20, c: 30) # a=1000, b=20, c=30
+
+# 以下も第一引数とみなされてしまう
+def foo(b, ...) # 第一引数だけ取り出して
+    bar(b*100, ...) # 別処理をすることができる
+end
+
+# こうすれば第二引数を取り出したことになる（書き方正しいかは？）
+def foo(a, b, ...)
+    bar(a, b*100, ...)
+end
